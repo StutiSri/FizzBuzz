e7d75850b5960b75c4cd71cebf3db2487dd10f25
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-10 02:03:57 +0530

    @ssrivastava2 - Implement return of Fizz for multiples of 3

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,6 +3,9 @@ public class FizzBuzz {
     private static final String FIZZ = "Fizz";
 
     String outputFor(int num) {
+        if (num % 3 == 0) {
+            return FIZZ;
+        }
         return null;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,7 +1,6 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
