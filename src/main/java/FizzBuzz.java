0d92c93ca77e6872a5a8b910dc0666dbfdcb44ae
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-10 02:10:33 +0530

    @ssrivastava2 - Implement return of Buzz for multiples of 5

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,11 +1,21 @@
 public class FizzBuzz {
 
     private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    public static final int FIZZ_FACTOR = 3;
+    public static final int BUZZ_FACTOR = 5;
 
     String outputFor(int num) {
-        if (num % 3 == 0) {
+        if (isMultipleOf(num, FIZZ_FACTOR)) {
             return FIZZ;
         }
+        if (isMultipleOf(num, BUZZ_FACTOR)) {
+            return BUZZ;
+        }
         return null;
+    }
+
+    private boolean isMultipleOf(int num, int factor) {
+        return num % factor == 0;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -11,14 +11,14 @@ class FizzBuzzTest {
     @Test
     void shouldReturnFizzForMultiplesOf3() {
         for (Integer num : Arrays.asList(3, 6, 9, 12)) {
-            assertEquals("Fizz", fizzBuzz.outputFor(num));
+            assertEquals("Fizz", fizzBuzz.outputFor(num), String.format("Expecting Fizz for %s", num));
         }
     }
 
     @Test
     void shouldReturnFizzForMultiplesOf5() {
-        for (Integer num : Arrays.asList(5, 10, 15)) {
-            assertEquals("Buzz", fizzBuzz.outputFor(num));
+        for (Integer num : Arrays.asList(5, 10, 20)) {
+            assertEquals("Buzz", fizzBuzz.outputFor(num), String.format("Expecting Buzz for %s", num));
         }
     }
 }
