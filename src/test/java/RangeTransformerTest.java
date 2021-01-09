dc00e555f654713e808d6b40542908492e1fab61
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-10 02:58:37 +0530

    @ssrivastava2 - Added test case for verifying tranformed list with size of 3 when the range is 10

--- src/test/java/RangeTransformerTest.java
+++ src/test/java/RangeTransformerTest.java
@@ -49,9 +49,7 @@ class RangeTransformerTest {
 
     @Test
     void shouldReturnListWithSize3ForRangeOf3() {
-        willReturn("1").given(numberTransformer).outputFor(1);
-        willReturn("SomeFunc").given(numberTransformer).outputFor(2);
-        willReturn("OtherFunc").given(numberTransformer).outputFor(3);
+        mockForRangeOf3();
 
         List<String> transformedRange = rangeTransformer.transform(3);
 
@@ -60,14 +58,56 @@ class RangeTransformerTest {
 
     @Test
     void shouldReturnListWithTransformedValueRangeOf3() {
-        willReturn("1").given(numberTransformer).outputFor(1);
-        willReturn("SomeFunc").given(numberTransformer).outputFor(2);
-        willReturn("OtherFunc").given(numberTransformer).outputFor(3);
+        mockForRangeOf3();
 
         List<String> transformedRange = rangeTransformer.transform(3);
 
         assertEquals("1", transformedRange.get(0));
         assertEquals("SomeFunc", transformedRange.get(1));
         assertEquals("OtherFunc", transformedRange.get(2));
+    }
+
+    @Test
+    void shouldReturnListWithSize10ForRangeOf10() {
+        mockForRangeOf10();
+
+        List<String> transformedRange = rangeTransformer.transform(10);
+
+        assertEquals(10, transformedRange.size());
+    }
+
+    @Test
+    void shouldReturnListWithTransformedValueRangeOf10() {
+        mockForRangeOf10();
+
+        List<String> transformedRange = rangeTransformer.transform(10);
+
+        assertEquals("1", transformedRange.get(0));
+        assertEquals("SomeFunc", transformedRange.get(1));
+        assertEquals("OtherFunc", transformedRange.get(2));
+        assertEquals("4", transformedRange.get(3));
+        assertEquals("5", transformedRange.get(4));
+        assertEquals("Other", transformedRange.get(5));
+        assertEquals("WhatThis", transformedRange.get(6));
+        assertEquals("8", transformedRange.get(7));
+        assertEquals("9", transformedRange.get(8));
+        assertEquals("10", transformedRange.get(9));
+    }
+
+    private void mockForRangeOf10() {
+        mockForRangeOf3();
+        willReturn("4").given(numberTransformer).outputFor(4);
+        willReturn("5").given(numberTransformer).outputFor(5);
+        willReturn("Other").given(numberTransformer).outputFor(6);
+        willReturn("WhatThis").given(numberTransformer).outputFor(7);
+        willReturn("8").given(numberTransformer).outputFor(8);
+        willReturn("9").given(numberTransformer).outputFor(9);
+        willReturn("10").given(numberTransformer).outputFor(10);
+    }
+
+    private void mockForRangeOf3() {
+        willReturn("1").given(numberTransformer).outputFor(1);
+        willReturn("SomeFunc").given(numberTransformer).outputFor(2);
+        willReturn("OtherFunc").given(numberTransformer).outputFor(3);
     }
 }
